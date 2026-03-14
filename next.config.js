f1a6142dd2d2020bc80ca66ada18a68ef3d6a8fb
alban <DA086872@stanislas.qc.ca>
2026-03-14 13:09:41 -0400

    Fix basePath for custom domain deployment

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? '/web-dev-agency-site' : '',
-  assetPrefix: isProd ? '/web-dev-agency-site/' : '',
 };
 
 module.exports = nextConfig;
