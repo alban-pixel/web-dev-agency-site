3c8d8a347d7a690a3320cd4e64bd5de82f96a1b6
alban <DA086872@stanislas.qc.ca>
2026-03-14 02:31:20 -0400

    🔧 Fix GH Pages build: compatibility with Next.js 13.0.3

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 const isProd = process.env.NODE_ENV === 'production';
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: isProd ? '/web-dev-agency-site' : '',
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? '/web-dev-agency-site' : '',
+  assetPrefix: isProd ? '/web-dev-agency-site/' : '',
 };
 
 module.exports = nextConfig;
