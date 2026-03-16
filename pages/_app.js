65c5e21753d7fe1e3e8f4cd5d48b5a83064de975
floragel <floragel@2024@@gmail.com>
2026-03-16 00:44:30 -0400

    fix: ultra-robust favicon implementation to bypass browser caching

--- pages/_app.js
+++ pages/_app.js
@@ -8,9 +8,10 @@ const MyApp = ({ Component, pageProps }) => (
   <LanguageProvider>
     <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico?v=5" />
-      <link rel="shortcut icon" href="/favicon.ico?v=5" />
-      <link rel="apple-touch-icon" href="/favicon.ico?v=5" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/maltos-icon.png?v=6" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/maltos-icon.png?v=6" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=6" />
+      <link rel="shortcut icon" href="/favicon.ico?v=6" />
       <link rel="preconnect" href="https://stijndv.com" />
     </Head>
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
